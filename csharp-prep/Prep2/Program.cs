8d7a3dbb034105e850ce68d862a59ca6bc14400d
Nortories <sooaemalelagij@gmail.com>
2023-01-06 12:31:33 -0700

    part1-2 fin

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage of grade did you get? ");
+        string grade = Console.ReadLine();
+
+        float fGrade = float.Parse(grade);
+        string letter;
+
+        if (fGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if(fGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if(fGrade >= 70)
+        {
+            letter = "C";
+        }        
+        else if(fGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter == "D" || letter == "F")
+        {
+            Console.Write($"You didnt pass with a grade of '{letter}' better luck next time");
+        }
+        else
+        {
+            Console.Write($"Congratulations you passed with a '{letter}'");
+        }
     }
 }
