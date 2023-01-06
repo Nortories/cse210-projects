4dc34178bc29fe56af91f2d54d3b836bd3272dbd
Nortories <sooaemalelagij@gmail.com>
2023-01-06 13:53:43 -0700

    pt 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,100);
+
+        Console.Write("Im thinking of a number between 1 and 100? ");
+
+        Console.WriteLine("Can you guess the magic number? ");
+        string guess = Console.ReadLine();
+        int guessNum = int.Parse(guess);
+
+        while (guessNum != magicNum)
+        {
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower!");
+                Console.Write("Can you guess the magic number? ");
+                guess = Console.ReadLine();
+                guessNum = int.Parse(guess);
+            }
+            else
+            {
+                Console.WriteLine("Higher!");
+                Console.Write("Can you guess the magic number? ");
+                guess = Console.ReadLine();
+                guessNum = int.Parse(guess);
+            }
+        }
+        
+        Console.Write("You guessed it!");
     }
 }
