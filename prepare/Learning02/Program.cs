ef9a2d6e75dbba4451dd054c934a40899a45c930
Nortories <sooaemalelagij@gmail.com>
2023-01-10 11:47:34 -0700

    W03 prep Fin

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 =new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle= "Software Engineer";
+        job1._startYear= 2020;
+        job1._endYear= 2023;
+
+        Job job2 =new Job();
+        job2._company = "Apple";
+        job2._jobTitle= "Genius";
+        job2._startYear= 2018;
+        job2._endYear= 2020;
+
+        Resume resume = new Resume();
+        resume._name = "Billy Bob";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
