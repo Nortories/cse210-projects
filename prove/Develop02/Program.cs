f7a297705c5487d90eec53b4ce3417927c141d02
Nortories <98294242+Nortories@users.noreply.github.com>
2023-01-12 11:32:10 -0700

    1st comit

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        StartingQu();
     }
+
+    // Menu
+    static void StartingQu()
+    {
+        Console.WriteLine("Please selcet one of the following choices");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("> ");
+        int selection = Convert.ToInt32(Console.ReadLine());
+        Menu(selection);
+    }
+
+    //Input Menu Logic
+    static void Menu(int selection)
+    {
+            if (selection == 1)
+            {
+                Write();
+            }
+            else if (selection == 2)
+            {
+                Display();
+            }
+            else if (selection == 3)
+            {
+                Load();
+            }
+            else if (selection == 4)
+            {
+                Save();
+            }
+            else if (selection == 5)
+            {
+                Quit();
+            }
+            else
+            {
+                System.Console.WriteLine("\n\nThat was not a valied input");
+                StartingQu();
+            }
+    }
+
+
+    //Menu options
+    static void Write()
+    {
+        Prompts prompts = new Prompts();
+        prompts.Prompt();
+        Console.ReadLine();
+    }
+
+    static void Display()
+    {
+        System.Console.WriteLine("display");
+    }
+
+    static void Load()
+    {
+        System.Console.WriteLine("load");
+    }
+
+    static void Save()
+    {
+        System.Console.WriteLine("save");
+    }
+
+    static void Quit()
+    {
+        System.Console.WriteLine("quit");
+    }
+
+
 }
