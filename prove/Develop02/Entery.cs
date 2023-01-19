fddf50d3ac0b8e666587cf5840b8cdc5c551277b
Nortories <sooaemalelagij@gmail.com>
2023-01-18 20:33:14 -0700

    1st real commit

--- prove/Develop02/Entery.cs
+++ prove/Develop02/Entery.cs
@@ -1,24 +1,43 @@
-
 public class Entery
 {
+    DateTime datetime = DateTime.Now;
 
-    public Entery()
+    public static string _title;
+    public static string _datetime;
+    public static string _entery; 
+
+
+    public void Prompt()
     {
+        Random rand = new Random();
+
+        // Add/Remove prompts
+        string[] prompts = 
+        {
+        "What did you eat for breakfast? ",
+        "How was the weather today? ",
+        "What is something new you learned? ",
+        "Where where you at lunch? ",
+        "What are your plans for tomorrow? "
+        };
+
+        //Creates list and populates from 'prompts'
+        List<string> promptsList = new List<string>(prompts);
+
+        //Generate a random number between 0 and elements in the list
+        int number = rand.Next(0, (1-(promptsList.Count)));
+
+        //Writes a random element from promptsList
+        Console.WriteLine($"{promptsList[number]}\n >");
     }
     
-    public string Text()
+     public void Write()
     {
-        Prompts prompts = new Prompts();
-        prompts.Prompt();
-
-        string entery;
-        entery = Console.ReadLine();
-
-        return entery;
+        System.Console.WriteLine("Entery Title >  ");
+        _title = Console.ReadLine();
+        System.Console.WriteLine("Entery >  ");
+        _entery = Console.ReadLine();
+        _datetime = datetime.ToString();
     }
 
-    private void Save()
-    {
-        
-    }
 }
